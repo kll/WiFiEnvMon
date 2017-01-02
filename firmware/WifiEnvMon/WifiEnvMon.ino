8754aa07b00d07c8b924da43375355be12cf3c65
Kevin Lewis <github@kevin.oakaged.io>
2017-01-02 15:14:34 -0500

    connect to SSD1306 I2C display with u8g2 library

--- firmware/WifiEnvMon/WifiEnvMon.ino
+++ firmware/WifiEnvMon/WifiEnvMon.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include <Wire.h>
+#include <U8g2lib.h>
 
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
+
+void setup()
+{
+  u8g2.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_ncenB14_tr);
+  u8g2.drawStr(0,20,"WifiEnvMon");
+  u8g2.sendBuffer();
 }
