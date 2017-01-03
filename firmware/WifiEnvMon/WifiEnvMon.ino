86492427e83c48fd07f2c8d230738981aadde934
Kevin Lewis <github@kevin.oakaged.io>
2017-01-03 08:11:59 -0500

    connect to WiFi using the WiFiManager library and show the IP

--- firmware/WifiEnvMon/WifiEnvMon.ino
+++ firmware/WifiEnvMon/WifiEnvMon.ino
@@ -1,9 +1,16 @@
 #include <Arduino.h>
+
 #include <Wire.h>
 #include <U8g2lib.h>
+
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+
+#include <ESP8266WiFi.h>          // ESP8266 Core WiFi Library
+#include <DNSServer.h>            // Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     // Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          // https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 
 #define DHTPIN    0
 #define DHTTYPE   DHT22 // options: DHT11 DHT21 DHT22
@@ -22,6 +29,9 @@ void setup()
   dht.begin();
 
   initDhtDelay();
+
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("WifiEnvMonAP", "WifiEnvMon");
 }
 
 void loop()
@@ -66,16 +76,20 @@ void updateDisplay()
   u8g2.setFont(u8g2_font_profont12_tf);
   
   u8g2.setCursor(0, 10);
-  u8g2.print("T: ");
+  u8g2.print("T : ");
   u8g2.print(temperatureEvent.temperature);
   u8g2.print("C / ");
   u8g2.print(temperatureEvent.temperature * 1.8 + 32);
   u8g2.print("F");
 
   u8g2.setCursor(0, 20);
-  u8g2.print("H: ");
+  u8g2.print("H : ");
   u8g2.print(humidityEvent.relative_humidity);
   u8g2.print("%");
+
+  u8g2.setCursor(0, 30);
+  u8g2.print("IP: ");
+  u8g2.print(WiFi.localIP());
   
   u8g2.sendBuffer();
 }
