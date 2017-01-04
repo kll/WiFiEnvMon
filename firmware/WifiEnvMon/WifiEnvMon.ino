f70204c7c3379758bbea0ed474f82af28f1de1c5
Kevin Lewis <github@kevin.oakaged.io>
2017-01-03 21:34:09 -0500

    use WiFiManager custom parameters to get additional configuration
    and store configuration on the file system

--- firmware/WifiEnvMon/WifiEnvMon.ino
+++ firmware/WifiEnvMon/WifiEnvMon.ino
@@ -1,3 +1,6 @@
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
+#include <ArduinoJson.h>
+
 #include <Arduino.h>
 
 #include <Wire.h>
@@ -17,26 +20,40 @@
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
+// default values that are overwritten if there are different values in config.json
+char mqtt_username[50] = "YOUR_USERNAME";
+char mqtt_key[50] = "YOUR_API_KEY";
+char sensor_name[50] = "UNIQUE_NAME_FOR_SENSOR";
+char mqtt_server[50] = "io.adafruit.com";
+char mqtt_port[6] = "1883";
+
+// flag for saving data
+bool shouldSaveConfig = false;
+
 unsigned long lastUpdate;
 uint32_t dhtDelayMS;
 sensors_event_t humidityEvent;
 sensors_event_t temperatureEvent;
 
+void saveConfigCallback()
+{
+  DEBUG_PRINTLN(F("Should save config"));
+  shouldSaveConfig = true;
+}
+
 void setup()
 {
   #ifdef DEBUGP
     Serial.begin(115200);
+    DEBUG_PRINTLN(F("In setup"));
   #endif
 
-  DEBUG_PRINTLN(F("In setup"));
-  
   u8g2.begin();
   dht.begin();
 
+  readConfig();
+  initWiFi();
   initDhtDelay();
-
-  WiFiManager wifiManager;
-  wifiManager.autoConnect("WifiEnvMonAP", "WifiEnvMon");
 }
 
 void loop()
@@ -52,9 +69,144 @@ void loop()
   updateDisplay();
 }
 
+void readConfig()
+{
+  //DEBUG_PRINTLN(F("formatting FS..."));
+  //SPIFFS.format();  // for testing
+    
+  DEBUG_PRINTLN(F("mounting FS..."));
+
+  if (SPIFFS.begin())
+  {
+    DEBUG_PRINTLN(F("mounted file system"));
+    
+    if (SPIFFS.exists("/config.json"))
+    {
+      //file exists, reading and loading
+      DEBUG_PRINTLN(F("reading config file"));
+      
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile)
+      {
+        DEBUG_PRINTLN(F("opened config file"));
+        
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+
+        #ifdef DEBUGP
+          json.printTo(Serial);
+        #endif
+        
+        if (json.success())
+        {
+          DEBUG_PRINTLN(F("\nparsed json"));
+
+          strcpy(sensor_name, json["sensor_name"]);
+          strcpy(mqtt_username, json["mqtt_username"]);
+          strcpy(mqtt_key, json["mqtt_key"]);
+          strcpy(mqtt_server, json["mqtt_server"]);
+          strcpy(mqtt_port, json["mqtt_port"]);
+
+          DEBUG_PRINTLN(F("setings copied from json"));
+        }
+        else
+        {
+          DEBUG_PRINTLN(F("failed to load json config"));
+        }
+      }
+    }
+  }
+  else
+  {
+    DEBUG_PRINTLN(F("failed to mount FS"));
+  }
+}
+
+void saveConfig()
+{
+  DEBUG_PRINTLN(F("saving config..."));
+    
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& json = jsonBuffer.createObject();
+  json["sensor_name"] = sensor_name;
+  json["mqtt_username"] = mqtt_username;
+  json["mqtt_key"] = mqtt_key;
+  json["mqtt_server"] = mqtt_server;
+  json["mqtt_port"] = mqtt_port;
+
+  #ifdef DEBUGP
+    json.printTo(Serial);
+  #endif
+
+  File configFile = SPIFFS.open("/config.json", "w");
+  if (configFile)
+  {
+    json.printTo(configFile);
+    configFile.close();
+    DEBUG_PRINTLN(F("save complete"));
+  }
+  else
+  {
+    DEBUG_PRINTLN(F("failed to open config file for writing"));
+  }
+}
+
+void initWiFi()
+{
+  DEBUG_PRINTLN(F("initializing WiFi..."));
+  
+  WiFiManagerParameter custom_sensor_name("name", "sensor name", sensor_name, 50);
+  WiFiManagerParameter custom_mqtt_username("username", mqtt_username, mqtt_username, 50);
+  WiFiManagerParameter custom_mqtt_key("key", mqtt_key, mqtt_key, 50);
+  WiFiManagerParameter custom_mqtt_server("server", mqtt_server, mqtt_server, 50);
+  WiFiManagerParameter custom_mqtt_port("port", mqtt_port, mqtt_port, 6);
+  
+  WiFiManager wifiManager;
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+  //wifiManager.setTimeout(120);
+  wifiManager.addParameter(&custom_sensor_name);
+  wifiManager.addParameter(&custom_mqtt_username);
+  wifiManager.addParameter(&custom_mqtt_key);
+  wifiManager.addParameter(&custom_mqtt_server);
+  wifiManager.addParameter(&custom_mqtt_port);
+  //wifiManager.resetSettings();  // for testing
+
+  if (!wifiManager.autoConnect("WifiEnvMonAP", "WifiEnvMon"))
+  {
+    DEBUG_PRINTLN(F("failed to connect and hit timeout"));
+    delay(3000);
+    ESP.reset();  // reset and try again
+    delay(5000);
+  }
+
+  DEBUG_PRINT(F("connected to AP: "));
+  DEBUG_PRINTLN(WiFi.SSID());
+  DEBUG_PRINT(F("local IP: "));
+  DEBUG_PRINTLN(WiFi.localIP());
+
+  // read updated parameters
+  strcpy(sensor_name, custom_sensor_name.getValue());
+  strcpy(mqtt_username, custom_mqtt_username.getValue());
+  strcpy(mqtt_key, custom_mqtt_key.getValue());
+  strcpy(mqtt_server, custom_mqtt_server.getValue());
+  strcpy(mqtt_port, custom_mqtt_port.getValue());
+  
+  // save the custom parameters to FS
+  if (shouldSaveConfig)
+  {
+    saveConfig();
+  }
+}
+
 void initDhtDelay()
 {
   DEBUG_PRINTLN(F("Reading required delay for DHT sensor."));
+  
   sensor_t sensor;
   dht.humidity().getSensor(&sensor);
   dhtDelayMS = sensor.min_delay / 1000;
@@ -107,7 +259,11 @@ void updateDisplay()
     u8g2.print("%");
   }
 
-  u8g2.setCursor(0, 30);
+  u8g2.setCursor(0, 54);
+  u8g2.print("N : ");
+  u8g2.print(sensor_name);
+  
+  u8g2.setCursor(0, 64);
   u8g2.print("IP: ");
   u8g2.print(WiFi.localIP());
   
